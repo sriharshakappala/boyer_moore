80b2d5e31f0cd31f50fd93d24bd08607438221bf
Colin Kelley <colindkelley@gmail.com>
2016-06-18 21:37:22 -0700

    Normalize %w[ ]

--- spec/boyer_moore_spec.rb
+++ spec/boyer_moore_spec.rb
@@ -3,18 +3,18 @@ require 'boyer_moore'
 describe BoyerMoore do
   describe BoyerMoore::Needle do
     it "should compute prefixes" do
-      expect(BoyerMoore::Needle::prefix(%w{A N P A N M A N})).to eq [0, 0, 0, 1, 2, 0, 1, 2]
-      expect(BoyerMoore::Needle::prefix(%w{f o o b a r})).to eq [0, 0, 0, 0, 0, 0]
+      expect(BoyerMoore::Needle::prefix(%w[A N P A N M A N])).to eq [0, 0, 0, 1, 2, 0, 1, 2]
+      expect(BoyerMoore::Needle::prefix(%w[f o o b a r])).to eq [0, 0, 0, 0, 0, 0]
     end
 
     it "should compute character_indexes" do
-      expect(BoyerMoore::Needle.new(%w{A N P A N M A N}).send(:character_indexes)).to eq({"A"=>6, "M"=>5, "N"=>7, "P"=>2})
-      expect(BoyerMoore::Needle.new(%w{f o o b a r}).send(:character_indexes)).to eq ({"a"=>4, "b"=>3, "o"=>2, "f"=>0, "r"=>5})
+      expect(BoyerMoore::Needle.new(%w[A N P A N M A N]).send(:character_indexes)).to eq({"A"=>6, "M"=>5, "N"=>7, "P"=>2})
+      expect(BoyerMoore::Needle.new(%w[f o o b a r]).send(:character_indexes)).to eq ({"a"=>4, "b"=>3, "o"=>2, "f"=>0, "r"=>5})
     end
 
     it "should implement good_suffixes" do
-      expect(BoyerMoore::Needle.new(%w{A N P A N M A N}).send(:good_suffixes)).to eq [6, 6, 6, 6, 6, 6, 3, 3, 1]
-      expect(BoyerMoore::Needle.new(%w{f o o b a r}).send(:good_suffixes)).to eq [6, 6, 6, 6, 6, 6, 1]
+      expect(BoyerMoore::Needle.new(%w[A N P A N M A N]).send(:good_suffixes)).to eq [6, 6, 6, 6, 6, 6, 3, 3, 1]
+      expect(BoyerMoore::Needle.new(%w[f o o b a r]).send(:good_suffixes)).to eq [6, 6, 6, 6, 6, 6, 1]
     end
   end
 
@@ -35,17 +35,17 @@ describe BoyerMoore do
   
   it "should match characters" do
     {
-      ['a', 'b', 'c'] => 0,
-      ['b', 'c', 'd'] => nil,
-      ['e', 'f', 'g'] => nil,
-      ['m', 'y', ' ', 'd', 'o', 'g', ' ', 'a', 'b', 'c'] => 7
+      %w[a b c] => 0,
+      %w[b c d] => nil,
+      %w[e f g] => nil,
+      %w[m y _ d o g _ a b c] => 7
     }.each do |haystack, position|
       expect(BoyerMoore.search(haystack, ['a', 'b', 'c'])).to eq position
     end
   end
 
   it "should match in the middle of a string" do
-    expect(BoyerMoore.search("xxxfoobarbazxxx".split(//), "foobar".split(//))).to eq 3
+    expect(BoyerMoore.search("xxxfoobarbazxxx".split(''), "foobar".split(''))).to eq 3
   end
 
   it "should match words" do
